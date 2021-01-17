b4fad07457c91971fef0783b5cd0f3cb0e5c9d53
吉田健人 <52515974+4456-kent@users.noreply.github.com>
2021-01-17 17:34:44 +0900

    Update gps.ino
    Software Serialを使うコードになっていたので2対のHardware Serialを使うコードに書き換え

--- test/GPS/gps.ino
+++ test/GPS/gps.ino
@@ -1,32 +1,30 @@
-#include <TinyGPS++.h>
-#include <SoftwareSerial.h>
+#include "TinyGPS++.h"
+
+#define RX_PIN 16
+#define TX_PIN 17
  
 TinyGPSPlus gps;
-SoftwareSerial mySerial(10, 11); // RX, TX
 //TinyGPSCustom magneticVariation(gps, "GPRMC", 10);
  
 void setup() {
  // Open serial communications and wait for port to open:
- Serial.begin(57600);
+ Serial.begin(115200);
  while (!Serial) {
  ; // wait for serial port to connect. Needed for native USB port only
  }
-
- 
- // set the data rate for the SoftwareSerial port
- mySerial.begin(9600);
- mySerial.println("Set the data rate");
+  Serial2.begin(9600,SERIAL_8N1,RX_PIN,TX_PIN);
+  Serial2.println("Set the data rate");
 }
  
 void loop() { // run over and over
- while (mySerial.available() > 0){
- char c = mySerial.read();
- //Serial.print(c);
- gps.encode(c);
- if (gps.location.isUpdated()){
- Serial.print("LAT="); Serial.println(gps.location.lat(), 6);
- Serial.print("LONG="); Serial.println(gps.location.lng(), 6);
- Serial.print("ALT="); Serial.println(gps.altitude.meters());
- }
+ while (Serial2.available() > 0){
+  char c = Serial2.read();
+  //Serial.print(c);
+  gps.encode(c);
+  if (gps.location.isUpdated()){
+    Serial.print("LAT="); Serial.println(gps.location.lat(), 6);
+    Serial.print("LONG="); Serial.println(gps.location.lng(), 6);
+    Serial.print("ALT="); Serial.println(gps.altitude.meters());
+  }
  }
 }
